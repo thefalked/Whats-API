ce31414c0baff5b1005a5ce8f6af82bbd0a9c8a8
Giuliano Crivelli <giuliano@agenciaw3.digital>
2019-10-08 07:47:48 -0400

    default project

---
+++ src/js/index.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  new ClipboardJS('#copy');
+
+  $("#generate").click(function () {
+    if ($("#generate").css("cursor") == "not-allowed") {
+      return
+    } else {
+      let pais = $("#pais").val().split("+")[1];
+      let ddd = $("#ddd").val();
+      let numero = $("#numero").val().split(".").join("").split("-").join("");
+      let texto = $("#texto").val().split(" ").join("%20");
+      let fullnumber = pais+ddd+numero;
+      let link;
+      if (texto.length > 0)
+        link = `https://wa.me/${fullnumber}?text=${texto}`;
+      else
+        link = `https://wa.me/${fullnumber}`;
+  
+      if ($("#pais").val() && $("#ddd").val() && $("#numero").val()) {
+        $("#textoModal").text(link);
+      }
+
+      $("#modal").modal("show");
+    }
+  });
+
+  $("#pais, #ddd, #numero").on("change paste keyup", function () {
+    if ($("#pais").val() && $("#ddd").val() && $("#numero").val() &&
+    $("#numero").val().split(".").join("").split("-").join("").length == 9) {
+      $("#generate").css("cursor", "pointer");
+    } else {
+      $("#generate").css("cursor", "not-allowed");
+    }
+  });
+});
